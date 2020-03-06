ddb075cf0bdde6c46a88b8a25d91638812c5c491
Amara Uzoma <amara1092@gmail.com>
2020-03-06 11:26:03 +0000

    Updated my Footer Bar

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React    from 'react';
+
+class Footer extends React.Component {
+
+  render() {
+    return (
+      <div className='jumbotron'>
+       <center>
+           <p2>CopyRight {'\u00A9'} Amara Uzoma</p2>
+           </center>
+      </div>
+    );
+  }
+
+}
+
+export default Footer;
